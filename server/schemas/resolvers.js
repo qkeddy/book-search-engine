d293881241456a488332571727b59b79f3e32c0d
Quin Eddy <qkeddy@gmail.com>
2022-05-22 11:06:24 -0400

    Installed the Apollo Server. Added typeDefs.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+// Import Mongoose data models
+const { Book, User } = require("../models");
+
+const resolvers = {
+
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+// Import Apollo GraphQL
+const { ggl } = require("apollo-server-express");
+
+// Setup type definitions
+const typeDefs = gql`
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks [String]
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        # Supports query of one or multiple users
+        users(_id: String): [User]
+
+        # Returns the profile of the person logged in
+        me: User
+    }
+
+    type Mutations {
+        # Create a user with username, email, password all required
+        createUser(
+            username: String!, 
+            email: String!, 
+            password: String!) : Auth
+
+        # Login user with the option to enter a username or email and require the password
+        login(
+            username: String, 
+            email: String, 
+            password: String!) : Auth
+        
+        # Save book to user profile
+        saveBook(bookId: String!) : User
+
+        # Delete book from user profile
+        deleteBook(bookId: String!) : User
+    }
+`;
+
+module.exports = typeDefs;
