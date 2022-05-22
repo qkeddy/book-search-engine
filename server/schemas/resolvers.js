70e135ba0451e9cbdf08e8da2b7e5b2dd711228e
Quin Eddy <qkeddy@gmail.com>
2022-05-22 11:22:47 -0400

    Added initial query of user(s)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,25 @@
 // Import Mongoose data models
 const { Book, User } = require("../models");
 
-const resolvers = {
+// Import Authentication libraries 
 
-}
+const resolvers = {
+    Query: {
+        user: (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return await User.find(params);
+        },
+    },
+
+    Mutation: {
+        createUser: async (parent, args) => {},
+
+        login: async (parent, args) => {},
+
+        saveBook: async (parent, args) => {},
+
+        deleteBook: async (parent, args) => {},
+    },
+};
 
 module.exports = resolvers;
