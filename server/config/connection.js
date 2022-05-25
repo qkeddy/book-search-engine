1f14d7343b3fe17136486fd8182c5ddf29b1ef90
Quin Eddy <qkeddy@gmail.com>
2022-05-25 11:06:07 -0400

    Added comment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+// Add `MONGODB_URI` to Heroku connection parameters
+// See: https://coding-boot-camp.github.io/full-stack/mongodb/deploy-with-heroku-and-mongodb-atlas
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
